b34b1f0d3efb3fa0907097657ca2db60507acf82
Valentim_Gab <gabrielvalentimxavier@gmail.com>
2023-04-27 23:56:05 -0300

    Lists with adjusted lists, but still no indentation #4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -38,6 +38,22 @@ public class Main {
         return "\t".repeat(Math.max(0, tabs));
     }
 
+    public static Object arrayConstructor(Object atrValue) {
+        List<String> updateList = new ArrayList<>();
+        for (Object item: ((List<?>) atrValue).toArray()) {
+            String itemStr = item.toString();
+            if (!(item instanceof Number || item instanceof Boolean)) {
+                if (item instanceof List<?>)
+                    item = arrayConstructor(item);
+                else
+                    item = ('"' + itemStr + '"');
+            }
+            updateList.add(item.toString());
+        }
+
+        return updateList;
+    }
+
     public static String jsonBuilder(Object o, int tabs) throws InvocationTargetException, IllegalAccessException {
         StringBuffer jsonString = new StringBuffer();
         char quotationMarks = '"';
@@ -53,32 +69,38 @@ public class Main {
                 Object atrValue = method.invoke(o);
                 String key = method.getName().substring(3, 4).toLowerCase() + method.getName().substring(4);
 
-                if (atrValue instanceof Number || atrValue instanceof List<?> || testMethodIsClass(atrValue))
+                if (atrValue instanceof Number || atrValue instanceof List<?> || atrValue == null
+                        || testMethodIsClass(atrValue) || atrValue instanceof Boolean)
                     quotationMarksValue = "";
                 else
                     quotationMarksValue = String.valueOf(quotationMarks);
 
                 if (atrValue instanceof List<?>) {
                     List<String> updateList = new ArrayList<>();
+
                     for (Object item: ((List<?>) atrValue).toArray()) {
                         String itemStr = item.toString();
+
                         if (!(item instanceof Number)) {
                             Object itemAux = item;
-                            if (testMethodIsClass(itemAux)) {                       
-                                item = jsonBuilder(itemAux, ++tabs);
-                                --tabs;
-                            } else {
+
+                            if (testMethodIsClass(itemAux))
+                                item = jsonBuilder(itemAux, tabs + 1);
+                            else if (item instanceof List<?>)
+                                item = arrayConstructor(item);
+                            else
                                 item = ('"' + itemStr + '"');
-                            }
                             updateList.add(item.toString());
                         } else {
                             break;
                         }
                         atrValue = updateList;
                     }
-                } else if (testMethodIsClass(atrValue)) {
-                    atrValue = jsonBuilder(atrValue, ++tabs);
-                    --tabs;
+                } else {
+                    if (atrValue != null && testMethodIsClass(atrValue)) {
+                        atrValue = jsonBuilder(atrValue, ++tabs);
+                        --tabs;
+                    }
                 }
                 jsonString.append(strTabs).append(quotationMarks).append(key).append(quotationMarks)
                         .append(": ").append(quotationMarksValue).append(atrValue).append(quotationMarksValue)
@@ -95,6 +117,7 @@ public class Main {
         User user = new User();
         Address address = new Address();
         City city = new City();
+
         user.setId(1);
         user.setName("Valentim");
         user.setCPF("444.444.444-44");
@@ -111,8 +134,12 @@ public class Main {
         user.getAddress().getCity().setState("Rio Grande do Sul");
         user.getAddress().getCity().setCountry("Brasil");
         user.getAddress().getCity().setBeaches(List.of("Praia 1", "Praia 2"));
+        user.setListInList(List.of(List.of("L1E1", "L1E2"), List.of("L2E1", "L2E2")));
+        user.setListInListNumber(List.of(List.of(1, 2), List.of(3, 4)));
+        user.setPcd(true);
 
         User father = new User();
+
         father.setId(2);
         father.setName("Sid");
         father.setCPF("555.555.555-55");
@@ -122,6 +149,7 @@ public class Main {
         father.setAddress(address);
 
         User mother = new User();
+
         mother.setId(3);
         mother.setName("Susi");
         mother.setCPF("666.666.666-66");
@@ -133,6 +161,7 @@ public class Main {
         user.setFamily(List.of(father, mother));
 
         String json = jsonBuilder(user, 1);
+
         System.out.println(json);
         showJson(json);
     }
